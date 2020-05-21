597002444b47a55180f8a45ee1c67c65a70a32c4
Elliott O'Hara <elliott.ohara@gmail.com>
2020-05-21 11:54:54 -0500

    DX-247: Allow linting of whole folder and allow changing of file in place.

--- src/dbt-command.ts
+++ src/dbt-command.ts
@@ -1,21 +1,40 @@
-import { readFileSync } from 'fs';
+import { readFileSync, fstat, writeFileSync } from 'fs';
 import * as program from 'commander';
 import formatter from './dbt-formatter';
+import * as glob from 'glob';
+import { Writer, StdOutWriter, FileWriter } from './core/writer';
+import { writer } from 'repl';
 
 program
-  .requiredOption('-f, --file <file>', 'sql file to format')
-  .option('--sql <flavour>', 'what sql flavor to use')
-  .option('--camelcase', 'allow camelcase')
-  .option('-i', '--indent <spaces>', 'how much to spaces for indentations')
-  .option('---no-upper', 'disable uppercasing reserved sql words')
+  .option('-f, --file <file>', 'sql file to format')
+  .option('-d, --directory <directory>', 'The directory to recursively search.')
+  .option('-i', '--indent <spaces>', 'number of spaces for indent. Defaults to 4.')
+  .option('--upper', 'upercase reserved sql words')
+  .option('--replace', 'overwrites the linted file(s).')
   .parse(process.argv);
 
-const query = readFileSync(program.file);
-const formatted = formatter(query.toString(), {
-  sql: program.sql || 'default',
-  indent: program.indent || 2,
-  upper: program.upper || true,
-  allowCamelcase: program.camelcase || false,
-});
+const writer_factory = (file:string): Writer => {
+  return program.replace ? new FileWriter(file) : new StdOutWriter()
+}
 
-process.stdout.write(formatted);
+const do_work = (file: string) => {
+  
+  let query = readFileSync(file);
+  let formatted = formatter(query.toString(), {
+    sql:  'default',
+    indent: program.indent || 4,
+    upper: program.upper,
+    allowCamelcase:  false,
+  });
+  let writer = writer_factory(file)
+  writer.write(formatted)
+  
+}
+
+if(program.file){
+  do_work(program.file);
+}else if (program.directory){
+  let all_files = glob.sync(`${program.directory}/**/*.sql`)
+  all_files.forEach(do_work)
+}
+
