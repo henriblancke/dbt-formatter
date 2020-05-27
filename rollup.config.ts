51b41de70e3f037b7566bddd92b6b337e2bbd1b5
Henri Blancke <blanckehenri@gmail.com>
2020-05-27 12:39:20 -0400

    [UPD] upgrade rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,40 +1,52 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import camelCase from 'lodash.camelcase';
-import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
 const pkg = require('./package.json');
 
-const libraryName = 'dbt-formatter';
+const cmdName = 'dbt-command';
+const libName = 'dbt-formatter';
 
-export default {
-  input: `src/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true }
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [
-    'lodash'
-  ],
-  watch: {
-    include: 'src/**'
+
+export default [
+  {
+    input: `src/${libName}.ts`,
+    output: {
+      name: camelCase(libName),
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript()
+    ]
   },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+  {
+    input: `src/${libName}.ts`,
+    external: ['fs'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript()
+    ]
+  },
+  {
+    input: `src/${cmdName}.ts`,
+    external: ['fs'],
+    output: [
+      { file: pkg.bin, format: 'cjs' }
 
-    // Resolve source maps to the original source
-    sourceMaps()
-  ]
-};
+    ],
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript()
+    ]
+  }
+];
